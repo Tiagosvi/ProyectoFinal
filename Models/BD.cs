98c16ee7daabd610195ba3650204ddd732c691e0
Tiagosvi <102734829+Tiagosvi@users.noreply.github.com>
2022-06-10 11:52:50 -0300

    Primer Commit

---
+++ Models/BD.cs
@@ -0,0 +1,38 @@
+using System;
+using Dapper;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Collections.Generic;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Models
+{
+    public static class BD
+    {
+        private static string _ConnectionString = "Server=A-BTA-08;Database=BaseProyecto;Trusted_Connection=True;";
+        private static string sql="";
+
+
+/*Select E1.Nombre AS Equipo1, P.Goles1, E2.Nombre AS Equipo2, P.Goles2  From Partidos P 
+Inner Join Equipos E1 on P.IdEquipo1 = E1.IdEquipo
+Inner Join Equipos E2 on P.IdEquipo2 = E2.IdEquipo
+inner join Fechas F on P.IdFecha = F.IdFecha
+Where F.Fecha = 1 AND F.IdLiga = 1
+ */
+    public static List<ResultadosModel> ListarResultados()
+        {
+            List<ResultadosModel> Lista = null;
+            using(SqlConnection db = new SqlConnection(_ConnectionString))
+            {
+                    sql = "Select E1.Nombre AS Equipo1, P.Goles1, E2.Nombre AS Equipo2, P.Goles2  From Partidos P "+
+                    "Inner Join Equipos E1 on P.IdEquipo1 = E1.IdEquipo"+
+Inner Join Equipos E2 on P.IdEquipo2 = E2.IdEquipo
+inner join Fechas F on P.IdFecha = F.IdFecha
+Where F.Fecha = 1 AND F.IdLiga = 1";
+                    Lista = db.Query<ResultadosModel>(sql).ToList();
+            }
+            return Lista;
+        }
+
+}
+}
---
+++ Models/ResultadosModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ProyectoFinal.Models
+{
+    public class ResultadosModel
+    {
+       
+
+        public string Equipo1 { get ; set ; }
+        public string Equipo2 { get ; set ; }
+        public int Goles1 { get ; set ; }
+        public int Goles2 { get ; set ; }
+        
+        
+    }
+}
